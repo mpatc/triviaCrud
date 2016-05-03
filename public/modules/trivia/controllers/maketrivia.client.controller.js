a4e7b13e28698189256069566b5a20a66a00b074
mpatc <matthewpclancy@gmail.com>
2016-05-03 00:21:19 -0700

    cruddy crud modal curd crudy crude

--- public/modules/trivia/controllers/maketrivia.client.controller.js
+++ public/modules/trivia/controllers/maketrivia.client.controller.js
@@ -4,5 +4,16 @@ angular.module('trivia').controller('MaketriviaController', ['$scope',
 	function($scope) {
 		// Controller Logic
 		// ...
+		$scope.open = function() {
+  $scope.showModal = true;
+};
+
+$scope.ok = function() {
+  $scope.showModal = false;
+};
+
+$scope.cancel = function() {
+  $scope.showModal = false;
+};
 	}
 ]);
